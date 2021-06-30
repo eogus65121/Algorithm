9b7229eaa2ec65275af20a4b82d53adee90fef48
eogus65121 <eogus6512@gmail.com>
2021-06-30 20:08:12 +0900

    Create 더맵게.java
    코테
    자바
    프로그래머스
    더 맵게

---
+++ 프로그래머스/java/더맵게.java
@@ -0,0 +1,28 @@
+package 프로그래머스.java;
+import java.util.PriorityQueue;
+
+public class 더맵게 {
+class Solution {
+    public static int solution(int [] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        for(int i : scoville) {
+            heap.add(i);
+        }
+        while(!heap.isEmpty()){
+            if(heap.peek() >= K) {
+                break;
+            } else if (heap.size() <2 && heap.peek() < K){
+                answer = -1;
+                break;
+            }
+            else {
+                int mix_scov = (heap.poll() + (heap.poll() *2));
+                heap.add(mix_scov);
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
+}
