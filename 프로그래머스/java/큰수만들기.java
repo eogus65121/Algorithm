7a51649ea4aea8844e7534795854650204aac76d
eogus65121 <eogus6512@gmail.com>
2021-07-07 22:25:19 +0900

    Create 큰수만들기.java
    코테
    자바
    프로그래머스
    큰 수 만들기

---
+++ 프로그래머스/java/큰수만들기.java
@@ -0,0 +1,23 @@
+package 프로그래머스.java;
+import java.util.Stack;
+
+public class 큰수만들기 {
+class Solution {
+    public String solution(String number, int k) {
+        char[] result = new char[number.length() - k];
+        Stack<Character> stack = new Stack<>();
+
+        for (int i=0; i<number.length(); i++) {
+            char c = number.charAt(i);
+            while (!stack.isEmpty() && stack.peek() < c && k-- > 0) {
+                stack.pop();
+            }
+            stack.push(c);
+        }
+        for (int i=0; i<result.length; i++) {
+            result[i] = stack.get(i);
+        }
+        return new String(result);
+    }
+}
+}
