88a3ccfdfd7ccc9fde78c44ba27cc3da37dd8315
eogus65121 <eogus6512@gmail.com>
2021-07-13 02:29:55 +0900

    Update _2016년.java
    update

--- 프로그래머스/java/_2016년.java
+++ 프로그래머스/java/_2016년.java
@@ -1,23 +1,21 @@
 package 프로그래머스.java;
 
 public class _2016년 {
-    class Solution {
-        public static String solution(int a, int b) {
-            String answer = "";
-            int [] month = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-            String [] day = {"SUN", "MON", "TUE", "WEN", "THU", "FRI", "SAT"};
-            int d_select = 0;
+    public static String solution(int a, int b) {
+        String answer = "";
+        int [] month = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String [] day = {"SUN", "MON", "TUE", "WEN", "THU", "FRI", "SAT"};
+        int d_select = 0;
             
-            for(int i = 0; i < a-1; i++){
-                d_select  = month[i] + 1;
-            }
-            answer = day[(d_select-1)%7];
-            return answer;
+        for(int i = 0; i < a-1; i++){
+            d_select  = month[i] + 1;
         }
-        public static void main(String[] args) {
-            int a = 5;
-            int b = 24;
-            solution(a, b);
+        answer = day[(d_select-1)%7];
+        return answer;
         }
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 24;
+        solution(a, b);
     }
 }
