cf3f2b3f2eb2792545cf879a432434bdb5b53455
eogus65121 <eogus6512@gmail.com>
2021-07-21 23:04:26 +0900

    Update 약수의개수와덧셈.java
    프로그래머스
    코테
    효율성 증가한 코드

--- 프로그래머스/java/약수의개수와덧셈.java
+++ 프로그래머스/java/약수의개수와덧셈.java
@@ -22,3 +22,24 @@ public class 약수의개수와덧셈 {
         }
     }
 }
+
+
+
+//효율성이 증가한 상태의 코드
+class Solution {
+    public int solution(int left, int right) {
+        int answer = 0;
+        int s = (int)Math.ceil(Math.sqrt(left));
+        int ss = s * s;
+        for (int i = left; i <= right; i++) {
+            if (i == ss) {
+                answer -= i;
+                s++;
+                ss = s * s;
+            } else {
+                answer += i;
+            }
+        }
+        return answer;
+    }
+}
