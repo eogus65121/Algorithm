36d18d5ae1e2690bd0a24d53607d46d7d0d1f315
eogus65121 <eogus6512@gmail.com>
2021-07-25 03:45:55 +0900

    Update 약수의개수와덧셈.java
    update

--- 프로그래머스/java/약수의개수와덧셈.java
+++ 프로그래머스/java/약수의개수와덧셈.java
@@ -1,23 +1,21 @@
 public class 약수의개수와덧셈 {
     class Solution{
-        public static int solution(int left, int right){
+        public int solution(int left, int right){
             int answer = 0;
     
-            for( ; left<=right; left++){
-                if(divisor(left) % 2 == 0) answer+=left;
+            for( ; left<=right; left++){ //left ~ right 반복문
+                if(divisor(left) % 2 == 0) answer+=left; //cnt 값이 홀수 / 짝수 판별
                 else answer -=left;
             }
-    
-            System.out.println(answer);
             return answer;
         }
     
-        public static int divisor(int num){
-            int cnt = 2;
+        //약수의 개수 구하기
+        public int divisor(int num){
+            int cnt = 2;  // 1과 본인을 제외하기 위한 cnt = 2
             for(int j = 2; j < num; j++){
-                if(num%j==0) cnt += 1;
+                if(num%j==0) cnt += 1; // 약수일 경우 cnt++
             }
-            System.out.println(cnt);
             return cnt;
         }
     }
@@ -26,7 +24,7 @@ public class 약수의개수와덧셈 {
 
 
 // 에라토스테네스의 체 코드를 참고하여 수정한 코드
-class Solution {
+class Solution2 {
     public int solution(int left, int right) {
         int answer = 0;
 
