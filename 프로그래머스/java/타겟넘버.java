63e7c96e93491538c2102f495491532ee232e5d6
eogus65121 <eogus6512@gmail.com>
2021-07-24 21:12:36 +0900

    Update 타겟넘버.java
    다른 풀이 update

--- 프로그래머스/java/타겟넘버.java
+++ 프로그래머스/java/타겟넘버.java
@@ -23,12 +23,37 @@ public class 타겟넘버 {
                     else sum += numbers[j];
                 }
                 if(sum == target) cnt++;
+                return;
             }
+
             for(int i = start; i < n; i++){
                 visited[i] = true;  // 뽑은 index == true 값으로 변경
                 combination(numbers, visited, i+1, n ,r-1, target); //재귀
                 visited[i] = false; // 뽑았던 index == false
             }
         }
+    }
+}
+
+//다른 사람 풀이
+
+class Solution1 { // dp 사용
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+        answer = dfs(numbers, 0, 0, target);  
+        return answer;
+    }
+                            //0       0   
+    int dfs(int[] numbers, int n, int sum, int target) {
+        if(n == numbers.length) {
+            if(sum == target) {
+                return 1; // target 값과 같을 때 cnt ++ 되는 값
+            }
+            return 0;
+        }
+        //dp를 사용하여 + 일 때와 - 일 때를 모두 구함, n == numbers.length가 될 때까지 
+        return dfs(numbers, n + 1, sum + numbers[n], target) + dfs(numbers, n + 1, sum - numbers[n], target); 
+
+        //주의!! numbers의 모든 값이 동일하기 때문에 사용할 수 있는 방법
     }
 }
