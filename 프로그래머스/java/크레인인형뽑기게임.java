ee3cd72e1b244d5cf44ce565838829884b1322ef
eogus65121 <eogus6512@gmail.com>
2021-08-05 02:40:42 +0900

    Update 크레인인형뽑기게임.java
    내 풀이 추가
    효율성 증가
    속도 증가
    가독성 증가

--- 프로그래머스/java/크레인인형뽑기게임.java
+++ 프로그래머스/java/크레인인형뽑기게임.java
@@ -40,3 +40,43 @@ public class 크레인인형뽑기게임 {
         }
     }
 }
+
+// 다시 풀어본 풀이
+
+class Solution {
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0; i < moves.length; i++){
+            int value = moves[i]-1;
+            int move_value = 0;
+            for(int j = 0; j<board.length; j++){
+                if(board[j][value] != 0) {
+                    move_value = board[j][value];
+                    board[j][value] = 0;
+                    break;
+                }
+            }
+            if(move_value != 0) answer += stack_cal(stack, move_value);
+        }
+        return answer;
+    }
+    public int stack_cal(Stack<Integer> stack, int n){
+        if(stack.isEmpty()) {
+            stack.push(n);
+            return 0;
+        }
+        else{
+            if(stack.peek() == n) {
+                stack.pop();
+                return 2;
+            }
+            else{
+                stack.push(n);
+                return 0;
+            }
+        }
+    }
+}
+
