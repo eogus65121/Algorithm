5d858877193963714d195ab59d089fc8877b08fb
eogus65121 <eogus6512@gmail.com>
2021-08-29 23:45:38 +0900

    Update 기능개발.java
    기능개발
    코테
    java
    다시풀기
    프로그래머스

--- 프로그래머스/java/기능개발.java
+++ 프로그래머스/java/기능개발.java
@@ -37,3 +37,43 @@ public class 기능개발 {
         }
     }
 }
+
+/*  다시 풀어본 코드
+import java.util.*;
+
+class Solution{
+    public static int[] solution(int[] progresses, int[] speeds) {
+        int[] answer;
+        Queue<Integer> q = new LinkedList<>(); // days 값을 저장하기 위한 q
+        List<Integer> list = new ArrayList<>(); // answer 배열로 변환하기 위한 list
+        for (int i = 0; i < progresses.length; i++) {
+            int days;
+            if ((100 - progresses[i]) % speeds[i] == 0) { // 0인 경우와 아닌경우 차이 생김
+                q.add((100 - progresses[i]) / speeds[i]);
+            } else {
+                q.add((100 - progresses[i]) / speeds[i] + 1);
+            }
+        }
+        int temp = q.poll();  
+        int cnt = 1;
+        while (true) {
+            if (!q.isEmpty()) {  //queue에 값이 없을 때까지 반복
+                if (temp >= q.peek()) {   // 값 비교 후 앞에 뽑았던 값보다 클 경우 list 저장
+                    q.poll();             // 아닐경우 pass
+                    cnt++;
+                } else {
+                    list.add(cnt);
+                    temp = q.poll();
+                    cnt = 1;
+                }
+            } else {
+                list.add(cnt);
+                break;
+            }
+        }
+        answer = new int[list.size()];  // list를 answer로 옮기는 작업
+        for(int i = 0; i < list.size(); i++) answer[i] = list.get(i);
+        return answer;
+    }
+}
+*/
