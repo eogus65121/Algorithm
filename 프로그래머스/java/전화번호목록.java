a8b9c7a61628e2aa886d9e44437d1b9f8a2e2d99
eogus65121 <eogus6512@gmail.com>
2021-08-29 23:43:46 +0900

    Update 전화번호목록.java
    프로그래머스
    전화목록
    java
    다시풀기

--- 프로그래머스/java/전화번호목록.java
+++ 프로그래머스/java/전화번호목록.java
@@ -24,3 +24,22 @@ public class 전화번호목록 {
         }
     }
 }
+
+/* 다시 풀어본 풀이
+import java.util.TreeMap;
+
+class Solution{
+    public static boolean solution(String[] phone_book){
+        boolean answer = true;
+        TreeMap<String, String> tm = new TreeMap<>(); // HashMap을 사용해도 무방
+        for(String i : phone_book) tm.put(i, i); // map에 값 추가
+        for(String str : phone_book){    // phone_book값을 가져와 해당 str 길이만큼 
+            for(int i = 0; i < str.length(); i++){  // substring 후 contains로 여부 확인
+                String comp = str.substring(0, i);
+                if(tm.containsKey(comp)) return answer = false;
+            }
+        }
+        return answer;
+    }
+}
+*/
