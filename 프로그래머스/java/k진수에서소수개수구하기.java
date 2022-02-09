d63637f4aea8a8e52456b9d0b4042c824500eac0
eogus65121 <eogus6512@gmail.com>
2022-02-10 00:54:02 +0900

    프로그래머스 코딩테스트 java k진수에서 소수 개수 구하기

--- 프로그래머스/java/k진수에서소수개수구하기.java
+++ 프로그래머스/java/k진수에서소수개수구하기.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class k진수에서소수개수구하기 {
+import java.util.*;
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        String cn = convNum(n, k);
+        String[] s_cn = cn.split("0");
+
+        for(String s : s_cn){
+            if(s.equals("") || s.equals("1")) continue;
+            if(isPrime(Long.parseLong(s))){
+                answer++;
+            }
+        }
+        return answer;
+    }
+    private String convNum(int n, int k){
+        StringBuilder sb = new StringBuilder();
+        while(n > k){
+            sb.append(n%k);
+            n /= k;
+        }
+        sb.append(Integer.toString(n));
+        return sb.reverse().toString();
+    }
+
+    private boolean isPrime(long num){
+        if(num == 2) return true;
+        for(int i = 3; i < Math.sqrt(num) + 1; i++){
+            if(num%i == 0) return false;
+        }
+        return true;
+    }
 }
