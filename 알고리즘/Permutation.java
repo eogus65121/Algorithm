0ed40e2c8557df09e6600f9215def5c6c80b2af9
eogus65121 <eogus6512@gmail.com>
2022-08-09 22:39:05 +0900

    순열 설명 추가

--- 알고리즘/Permutation.java
+++ 알고리즘/Permutation.java
@@ -20,7 +20,8 @@ public class Permutation{
             System.out.println();
         }
     }
-
+    /** rtnList는 파라미터에서 제외하고 싱글톤으로 선언해 데이터를 넣는 것도 가능 */
+    /** 주의! rtnList에 데이터를 add할 때 arr.clone()로 저장해주지 않으면 자동 정렬을 해서 데이터가 저장됨... 왜 그런지는 나도 잘.... */
     private static void permutationtt(String[] arr, int endPoint, String[] memo, boolean[] vst, int depth, List<String[]> rtnList){
         if(endPoint == depth){
             rtnList.add(memo.clone());
