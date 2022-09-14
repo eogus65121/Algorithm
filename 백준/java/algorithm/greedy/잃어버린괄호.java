9a82ac9dc1b67d635d69bf02aaff8dfcd2db94ec
eogus65121 <eogus6512@gmail.com>
2022-09-14 22:03:21 +0900

    백준 java 그리디 알고리즘 거스름돈

---
+++ 백준/java/algorithm/greedy/잃어버린괄호.java
@@ -0,0 +1,25 @@
+package 백준.java.algorithm.greedy;
+import java.util.*;
+import java.io.*;
+
+public class 잃어버린괄호 {
+
+    public class Main {
+        public static void main(String[] args) throws IOException{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String str = br.readLine();
+            int answer = 0;
+            String[] split1 = str.split("-");
+            for (int i = 0; i < split1.length; i++) {
+                int num = 0;
+                StringTokenizer st = new StringTokenizer(split1[i], "+");
+                while (st.hasMoreTokens()) {
+                    num += Integer.parseInt(st.nextToken());
+                }
+                if (i != 0) answer -= (num);
+                else answer += num;
+            }
+            System.out.println(answer);
+        }
+    }
+}
