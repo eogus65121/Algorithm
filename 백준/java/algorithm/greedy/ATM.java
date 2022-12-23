b5ec68a2f93bba97a789c2fcfd6682f69e6cc614
eogus65121 <eogus6512@gmail.com>
2022-12-23 22:39:15 +0900

    ATM 백준 코테 그리디

---
+++ 백준/java/algorithm/greedy/ATM.java
@@ -0,0 +1,28 @@
+package 백준.java.algorithm.greedy;
+
+import java.util.*;
+import java.io.IOException;
+import java.util.stream.Collectors;
+
+class ATM {
+    public static void main(String[] args) throws IOException{
+        // 사람 인원 수 n
+        // i번 사람이 돈을 인수하는데 걸리는 시간은 pi분이다
+        // 줄을 서는 순서에 따라 돈을 인출하는데 필요한 시간의 합이 달라진다 (대기 시간이 있다는 말)
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i =0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        int answer = 0, delay = 0;
+        for(int i = 0; i < arr.length; i++){
+            answer += delay + arr[i];
+            delay += arr[i];
+        }
+
+        System.out.print(answer);
+    }
+
+}
