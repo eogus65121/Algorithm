5415414ae73d1c1dbb24caa068631cc12d07f6ab
eogus65121 <eogus6512@gmail.com>
2022-12-25 20:20:37 +0900

    java 코테 백준 로프 카카오 기출

---
+++ 백준/java/algorithm/greedy/로프.java
@@ -0,0 +1,28 @@
+package 백준.java.algorithm.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class 로프{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int cnt = Integer.parseInt(st.nextToken());
+        int arr[] = new int[cnt];
+        for(int i=0; i < cnt; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        long max = 0;
+        for(int i = cnt-1; i >= 0; i--) {
+            arr[i] = arr[i] * (cnt-i);
+            if(max < arr[i]) max = arr[i];
+        }
+        System.out.println(max);
+    }
+}
